ebf1f02d4a1740bc577526768cc974856cab5ea9
somyapradhanfw25051 <121215804+somyapradhanfw25051@users.noreply.github.com>
2023-01-15 22:56:46 +0530

    Add files via upload
    prime

---
+++ git1.js
@@ -0,0 +1,18 @@
+//Print Prime Numbers from 1 to given limit
+
+console.log(" 1 is independant number");
+for(let i=2;i<=10;i++){
+  let num = i;
+  let count=0;
+  for(let j=2; j<=num;j++){
+    if(num%j==1){
+    count++;
+  }
+}
+if(count==1){
+  console.log(i,"is a prime number");
+}
+else{
+  console.log(i,"not a prime number");
+}
+}
